7152347610ee58bd87a5c1104082ea068f8ca2ce
Marc Dwyer <marcdwyer1738@gmail.com>
2019-11-26 14:57:54 -0600

    added debounce to listen for scroll evt, adds evtlistener to newly loaded divs

--- index.js
+++ index.js
@@ -9,21 +9,37 @@ function disableVideos() {
     }
   }
 }
+
 function listener() {
-  let playableDivs = document.querySelectorAll("div.slider-item");
+  const playableDivs = document.querySelectorAll("div.slider-item");
   console.log(playableDivs.length);
   if (playableDivs.length) {
     for (let x = 0; x < playableDivs.length; x++) {
       const pDiv = playableDivs[x];
+      if (pDiv.isStopped) continue
       pDiv.addEventListener("transitionend", e => {
         const vid = pDiv.querySelector("video");
         if (vid) {
+          pDiv.isStopped = true
           vid.src = "";
         }
       });
     }
   }
 }
+const debounce = (func, dur) => {
+  let timer;
+  return function () {
+    const ctx = this,
+      args = arguments
 
+    clearTimeout(timer)
+    time = setTimeout(() => {
+      func.apply(ctx, args)
+    }, dur)
+  }
+}
 setTimeout(disableVideos, 1500);
 listener();
+
+document.addEventListener("scroll", debounce(listener, 650))
