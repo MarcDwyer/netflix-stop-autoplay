c30f6453b4439b8ba776ae5e6c280773fab4ae2d
Marc Dwyer <marcdwyer1738@gmail.com>
2019-11-26 20:30:27 -0600

    loooookin goooood

---
+++ dist/content.js
@@ -0,0 +1,45 @@
+const notStr = ":not([isStopped=true])";
+function attachListener(ele) {
+    if (NodeList.prototype.isPrototypeOf(ele)) {
+        //@ts-ignore
+        ele = [...ele];
+    }
+    for (let x = 0; x < ele.length; x++) {
+        const pDiv = ele[x];
+        pDiv.addEventListener("transitionend", () => {
+            const vid = pDiv.querySelector("video");
+            pDiv.setAttribute("isStopped", "true");
+            if (vid) {
+                vid.src = "";
+            }
+        });
+    }
+}
+function initListener() {
+    const billboard = document.querySelector(`.billboard`);
+    if (!billboard)
+        return;
+    attachListener([
+        billboard,
+        ...document.querySelectorAll("div[class*='video']")
+    ]);
+}
+function listenNewDivs() {
+    const playableDivs = document.querySelectorAll(`div.slider-item${notStr}`);
+    console.log(playableDivs.length);
+    attachListener(playableDivs);
+}
+const debounce = (func, dur) => {
+    let timer;
+    return function () {
+        const ctx = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(ctx, args);
+        }, dur);
+    };
+};
+initListener();
+listenNewDivs();
+document.addEventListener("scroll", debounce(listenNewDivs, 650));
+//# sourceMappingURL=content.js.map
