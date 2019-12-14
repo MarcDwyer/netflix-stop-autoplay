6562d97c700179443735dc56458b2fdfa61fd6b2
Marc Dwyer <marcdwyer1738@gmail.com>
2019-12-13 22:10:04 -0600

    converted class into regular functions, more functional easier debugging

--- src/content.ts
+++ src/content.ts
@@ -1,21 +1,3 @@
-const NODEINSERTED = Symbol("newnode"),
-  ONCLICK = Symbol("click");
-
-type EventPayload = {
-  list: Element[] | NodeListOf<Element>;
-  type: Symbol;
-};
-type IMyEvents = {
-  addTransitionEvt(ele: Element): void;
-  addClickEvt(ele: Element): void;
-};
-type MyQueries = {
-  getDivs?: string;
-  getSpans?: string;
-  getBillboard?: string;
-  subBillboard?: string;
-  getVideos?: string;
-};
 const debounce = (
   func: Function,
   dur: number
@@ -35,9 +17,6 @@ const getNewDivs = (query: string, not?: boolean): Element[] => {
   query = not ? query + ":not([stopped=true])" : query;
 
   const queryResult = document.querySelectorAll(query);
-  if (!queryResult.length) {
-    return [];
-  }
   return [...queryResult];
 };
 const addTransitionEvt = (element: Element) => {
@@ -49,46 +28,29 @@ const addTransitionEvt = (element: Element) => {
     }
   });
 };
-class NetflixListener {
-  public queries: MyQueries;
-  constructor() {
-    this.queries = {
-      getDivs: "div.slider-item",
-      getSpans: "span.handle",
-      getBillboard: "div.billboard",
-      subBillboard: "div.billboard-row",
-      getVideos: "div[class*='video']"
-    };
-  }
 
-  attachListeners = (payload: Element[]) => {
-    if (!payload.length) return;
-    for (const ele of payload) {
-      addTransitionEvt(ele);
-    }
-  };
-  listenNewMedia = (e?: MutationEvent) => {
+const queries = ["div.slider-item", "span.handle", "div.billboard", "div.billboard-row", "div[class*='video']", "div.background"]
+
+const listenNewMedia = (e?: MutationEvent) => {
+  //@ts-ignore
+  if (
+    e &&
+    e.srcElement &&
     //@ts-ignore
-    if (
-      e &&
-      e.srcElement &&
-      //@ts-ignore
-      /image-rotator-image/g.test(e.srcElement.classList.value)
-    )
-      return;
-    const playableDivs = Object.values(this.queries);
-    const queryResults = playableDivs
-      .map(divStr => getNewDivs(divStr, true))
-      //@ts-ignore
-      .flat(Infinity);
-    if (!playableDivs) return;
-    this.attachListeners(queryResults);
-  };
-}
-const controlFlix = new NetflixListener();
-controlFlix.listenNewMedia();
-
+    /image-rotator-image/g.test(e.srcElement.classList.value)
+  )
+    return;
+  const queryResults = queries
+    .map(divStr => getNewDivs(divStr, true))
+    //@ts-ignore
+    .flat(Infinity);
+  if (!queryResults.length) return
+  for (const ele of queryResults) {
+    addTransitionEvt(ele)
+  }
+};
+listenNewMedia()
 document.addEventListener(
   "DOMNodeInserted",
-  debounce(controlFlix.listenNewMedia, 650)
+  debounce(listenNewMedia, 650)
 );
