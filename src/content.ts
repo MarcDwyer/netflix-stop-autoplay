2cd2ac4f333df1b427a4aab64173e39c81263584
Marc Dwyer <marcdwyer1738@gmail.com>
2019-12-17 16:44:48 -0600

    changed debounce timer

--- src/content.ts
+++ src/content.ts
@@ -79,4 +79,4 @@ const listenNewMedia = (e?: MutationEvent) => {
   addTransitionEvts(getNodes);
 };
 listenNewMedia();
-document.addEventListener("DOMNodeInserted", debounce(listenNewMedia, 450));
+document.addEventListener("DOMNodeInserted", debounce(listenNewMedia, 350));
