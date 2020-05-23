3049011bbfcaf4d18ba29ba886252a6cfa8ecc7e
Marc Dwyer <marcdwyer1738@gmail.com>
2020-05-23 18:33:03 -0500

    reverted back to old tsconfig settings

---
+++ src/content.ts
@@ -0,0 +1,80 @@
+const debounce = (
+  func: Function,
+  dur: number
+): EventListenerOrEventListenerObject => {
+  let timer: number | undefined;
+  return function () {
+    const ctx = this,
+      args = arguments;
+
+    clearTimeout(timer);
+
+    //@ts-ignore
+    timer = setTimeout(() => {
+      func.apply(ctx, args);
+    }, dur);
+  };
+};
+
+// These queries are elements that have auto-playing videos as children
+const queries = {
+  billBoardQueries: ["div.billboard", "div.billboard-row"],
+  regQueries: ["div.slider-item", "div[class*='video']", "div.background"],
+  profileScreen: "div.list-profiles",
+  profileCards: "div.profile-icon",
+};
+const getElements = (queries: string | string[]): Element[] => {
+  if (typeof queries === "string") {
+    queries = [queries];
+  }
+  // An additional query that prevents us from selecting already tagged elements
+  const not = ":not([stopped=true])";
+  const results = [];
+  for (const query of queries) {
+    results.push(...document.querySelectorAll(query + not));
+  }
+  return results;
+};
+// Tag elements with an attribute and eventListener
+const tagElement = (
+  eles: Element[],
+  eventType: string,
+  func: (ele: Element) => void
+) => {
+  for (const ele of eles) {
+    ele.addEventListener(eventType, () => func(ele));
+    ele.setAttribute("stopped", "true");
+  }
+};
+// Sometimes when logging into Netflix you must choose which profile to select
+// Need a special function to handle such a case.
+const handleInit = () => {
+  const { profileScreen } = queries;
+  const checkPofile = getElements(profileScreen);
+  if (checkPofile && checkPofile.length) {
+    tagElement(checkPofile, "click", () => setTimeout(scan, 450));
+  }
+};
+
+const scan = (init?: boolean) => {
+  if (init) {
+    handleInit();
+    return;
+  }
+  const { regQueries, billBoardQueries } = queries;
+
+  const tagThese = getElements([...regQueries, ...billBoardQueries]);
+
+  tagElement(tagThese, "transitionstart", (ele: Element) => {
+    const vid = ele.querySelector("video");
+    if (vid) {
+      vid.src = "";
+    }
+  });
+};
+
+scan(true);
+document.addEventListener(
+  "DOMNodeInserted",
+  debounce(() => scan(), 450)
+);
