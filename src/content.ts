2c13fa94687c61b8a4fd2e951646af0654b608b1
Marc Dwyer <marcdwyer@marcs-mbp.attlocal.net>
2020-09-02 15:28:36 -0500

    a lot less code

--- src/content.ts
+++ src/content.ts
@@ -1,6 +1,6 @@
 const debounce = (
   func: Function,
-  dur: number
+  dur: number,
 ): EventListenerOrEventListenerObject => {
   let timer: number | undefined;
   return function () {
@@ -16,53 +16,15 @@ const debounce = (
   };
 };
 
-// These queries are elements that have auto-playing videos as children
-const queries = {
-  billBoardQueries: ["div.billboard", "div.billboard-row"],
-  regQueries: ["div.slider-item", "div[class*='video']", "div.background"],
-  profileScreen: "div.list-profiles",
-  profileCards: "div.profile-icon",
-};
-
-const getElements = (queries: string | string[]): Element[] => {
-  if (typeof queries === "string") {
-    queries = [queries];
+function stopAutoplay() {
+  if (document.location.href.includes("watch")) {
+    return;
   }
-  // An additional query that prevents us from selecting already tagged elements
-  const not = ":not([stopped=true])";
-  const results = [];
-  for (const query of queries) {
-    results.push(...document.querySelectorAll(query + not));
+  const vl = document.querySelectorAll("video");
+  for (const v of vl) {
+    v.src = "";
   }
-  return results;
-};
-// Tag elements with an attribute and eventListener
-const tagElement = (
-  eles: Element[],
-  eventType: string,
-  func: (ele: Element) => void
-) => {
-  for (const ele of eles) {
-    ele.addEventListener(eventType, () => func(ele));
-    ele.setAttribute("stopped", "true");
-  }
-};
+}
 
-const scan = () => {
-  const { regQueries, billBoardQueries, profileScreen } = queries;
-  const checkPofile = getElements(profileScreen);
-  if (checkPofile && checkPofile.length) {
-    tagElement(checkPofile, "click", () => setTimeout(scan, 450));
-  }
-
-  const tagThese = getElements([...regQueries, ...billBoardQueries]);
-  tagElement(tagThese, "transitionstart", (ele: Element) => {
-    const vid = ele.querySelector("video");
-    if (vid) {
-      vid.src = "";
-    }
-  });
-};
-
-window.addEventListener("load", scan);
-document.addEventListener("DOMNodeInserted", debounce(scan, 450));
+window.addEventListener("load", stopAutoplay);
+document.addEventListener("DOMNodeInserted", debounce(stopAutoplay, 250));
